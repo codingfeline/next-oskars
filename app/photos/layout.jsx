836c4cf5be8ca66158b51ecd335fecbf23983df5
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-15 06:35:19 +0100

    photos, own layout

---
+++ app/photos/layout.jsx
@@ -0,0 +1,5 @@
+const Layout = ({ children }) => {
+  return <div className="flex flex-col h-screen bg-red-100">{children}</div>
+}
+
+export default Layout
---
+++ app/photos/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h2 className="head_text text-center gradient">photos</h2>
+    </section>
+  )
+}
+
+export default About
