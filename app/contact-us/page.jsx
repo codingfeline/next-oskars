99884ebd9d7d97927bc03a6d4224870f7374f137
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-16 06:18:18 +0100

    form skeleton

--- app/contact-us/page.jsx
+++ app/contact-us/page.jsx
@@ -1,7 +1,27 @@
 const Contact = () => {
   return (
-    <div className="mb-auto">
+    <div className="flex flex-col mb-auto items-center">
       <h3>contact us</h3>
+      <form className="w-3/5">
+        <div>
+          <label htmlFor="name">
+            Name <input type="text" id="name" />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="email">
+            Email
+            <input type="text" id="email" />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="message">
+            Message{' '}
+            <textarea name="" id="message" cols="30" rows="10"></textarea>
+          </label>
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   )
 }
