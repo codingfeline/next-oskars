57e6745aed1e681c832d698e63a130bdee9ff28b
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-16 06:31:38 +0100

    intro, description

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
 export default function Home() {
   return (
-    <main className="flex min-h-scre mb-auto flex-co items-center justify-betwee">
-      home
-      <input type="text" />
+    <main className="flex mb-auto flex-col items-center justify-around h-screen ">
+      <div className="flex flex-col justify-around items-center h-80  bg-slate-300 w-full sm:w-4/5 sm:rounded-xl px-4">
+        <h2>Great friendly family barber shop</h2>
+        <h3>by professional staff for all styles</h3>
+        <h4>Hair cuts, Shape-Ups, razo shave & hot towel, beard trim</h4>
+        <h5>No appointment needed, just pop in</h5>
+      </div>
     </main>
   )
 }
