4f1f03c6fa259a6d683f65d8ee9b38f6cffa0339
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-18 22:37:31 +0100

    grow

---
+++ app/page.jsx
@@ -0,0 +1,21 @@
+import OpeningHours from '@components/Hours'
+import Prices from '@components/Prices'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center mb-auto bg-lime-100 grow">
+      <main className="flex flex-col  items-center">
+        <div className="flex flex-col justify-between items-center h-50  bg-slate-300 w-full sm:w-4/5 sm:rounded-xl p-8">
+          <h2>Great friendly family barber shop</h2>
+          <h3>by professional staff for all styles</h3>
+          <h4>Hair cuts, Shape-Ups, razo shave & hot towel, beard trim</h4>
+          <h5>No appointment needed, just pop in</h5>
+        </div>
+      </main>
+      {/* <section>
+         <OpeningHours />
+        <Prices /> 
+      </section> */}
+    </div>
+  )
+}
---
+++ app/photos/page.jsx
@@ -0,0 +1,48 @@
+const Photos = () => {
+  return (
+    <section className="flex flex-col mb-auto grow justify-center items-center">
+      <h2>photos..</h2>
+      <p className="text-2xl hidden">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quam quidem
+        commodi fugit in veniam, enim dolorum voluptatum fuga vel animi ut eius
+        illo? Velit ab doloribus ullam laborum esse officiis eius dicta labore.
+        quod, neque harum? A harum mollitia qui doloremque ab impedit vitae amet
+        ducimus alias, architecto est rerum commodi quod, id labore accusantium
+        dolorum necessitatibus eos doloribus cumque voluptate asperiores. Animi
+        distinctio incidunt odit sed aspernatur ipsam, quaerat similique quos
+        nulla hic cumque blanditiis non quo asperiores doloribus nobis quisquam
+        excepturi praesentium, natus dolorum! Obcaecati, non tempora excepturi
+        sit earum quaerat maxime nihil! Aliquam maxime tempora sit officia,
+        possimus amet sequi soluta quibusdam deleniti minima recusandae
+        doloremque blanditiis quaerat autem ducimus minus, provident, officiis
+        alias rerum iusto! Qui vitae repudiandae sequi unde alias enim facilis,
+        non quisquam sunt harum a doloremque repellat nam nihil cumque
+        excepturi, reprehenderit, nostrum iusto. Eligendi error odit repellendus
+        ratione aut assumenda perspiciatis dolorum nam aperiam non maiores
+        doloremque, vero eos animi nesciunt consequatur cum esse voluptatibus
+        doloribus dolores hic nostrum totam velit. Incidunt voluptates minus
+        excepturi sint odio. Numquam quae incidunt dolores iure officiis.
+        Officiis doloremque omnis nemo sint exercitationem ab, aut architecto
+        magni. Repudiandae laudantium hic odit explicabo voluptas id cumque nemo
+        temporibus ducimus numquam quo consequuntur deserunt libero a fuga, eos
+        praesentium totam ipsum. Aspernatur harum iste velit quasi cumque? Nisi
+        corporis ipsum libero vitae consectetur possimus molestias nam
+        laudantium consequatur ratione. Unde magnam qui cumque aliquid ut labore
+        assumenda autem. Animi quae quam deserunt laboriosam suscipit quis
+        possimus magnam rerum reprehenderit minima debitis mollitia impedit
+        accusamus tempora sunt quos delectus, ullam blanditiis velit iure
+        consectetur illo? Eaque, fugiat nisi quo iusto sequi sunt, deserunt eius
+        culpa iure, odio tenetur! Consectetur in qui saepe a. Omnis voluptates
+        molestias nesciunt amet eos laboriosam accusamus maiores, illum dolor.
+        Officiis molestias itaque ea eius, vitae a libero, aliquid aut nulla et
+        tempora iure consectetur nostrum, illum mollitia dolores illo! Quasi
+        consequuntur quidem expedita architecto sed incidunt facilis maiores
+        repellat recusandae dolor perferendis, praesentium quisquam dolorem quae
+        qui sit eaque voluptatibus facere minus? Recusandae consectetur natus
+        quia alias laboriosam temporibus!
+      </p>
+    </section>
+  )
+}
+
+export default Photos
