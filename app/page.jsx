9c9f3859f6465882292ee0bb2d83a86568ea5ee1
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-05-30 23:34:19 +0100

    full screen each section

--- app/page.jsx
+++ app/page.jsx
@@ -3,19 +3,21 @@ import Prices from '@components/Prices'
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center mb-auto bg-lime-100 grow">
-      <main className="flex flex-col  items-center">
-        <div className="flex flex-col justify-between items-center h-50  bg-slate-300 w-full sm:w-4/5 sm:rounded-xl p-8">
+    <div className="flex flex-col flex_center overflow-auto  scroll_parent  ">
+      <main className="flex_center h-screen bg-lime-100 snap-start">
+        <div className="flex flex-col justify-between items-center h-50  bg-slate-300 w-full sm:w-4/5 sm:rounded-xl p-8 ">
           <h2>Great friendly family barber shop</h2>
           <h3>by professional staff for all styles</h3>
           <h4>Hair cuts, Shape-Ups, razo shave & hot towel, beard trim</h4>
           <h5>No appointment needed, just pop in</h5>
         </div>
       </main>
-      {/* <section>
-         <OpeningHours />
-        <Prices /> 
-      </section> */}
+      <section className="flex_center h-screen bg-slate-100 w-full border snap-start ">
+        <OpeningHours />
+      </section>
+      <section className="flex_center h-screen bg-lime-100 w-full snap-start ">
+        <Prices />
+      </section>
     </div>
   )
 }
