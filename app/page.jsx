e404e30f6b50684ff5454b04b6a48ba8b1c02caa
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-06-04 12:36:54 +0100

    snap mandatory

--- app/page.jsx
+++ app/page.jsx
@@ -3,10 +3,12 @@ import Prices from '@components/Prices'
 
 export default function Home() {
   return (
-    <div className="scroll_parent h-screen  ">
+    <div className=" h-screen overflow-y-scroll snap-y snap-mandatory ">
       <main className="flex_center h-screen bg-lime-100 snap-start">
         <div className="flex flex-col justify-between items-center h-50  bg-slate-300 w-full sm:w-4/5 sm:rounded-xl p-8 ">
-          <h2>Great friendly family barber shop</h2>
+          <h2 className="animate__animated animate__bounce">
+            Great friendly family barber shop
+          </h2>
           <h3>by professional staff for all styles</h3>
           <h4>Hair cuts, Shape-Ups, razo shave & hot towel, beard trim</h4>
           <h5>No appointment needed, just pop in</h5>
