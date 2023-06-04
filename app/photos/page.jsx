6d4a4c852dcc15eb6f5bf5af9a980786103f81c3
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-06-04 12:35:45 +0100

    refactor css

--- app/photos/page.jsx
+++ app/photos/page.jsx
@@ -1,3 +1,6 @@
+import '@assets/photos.css'
+import Image from 'next/image'
+
 const Photos = () => {
   return (
     <div className="flex_screen">
@@ -15,19 +18,19 @@ const Photos = () => {
             <div className="box photo1">
               <div className="content_box">
                 <h2>photo 1</h2>
-                <p>lorem50</p>
+                <Image src="/photo1.webp" width={700} height={700} />
               </div>
             </div>
             <div className="box photo2">
               <div className="content_box">
                 <h2>photo 2</h2>
-                <p>lorem50</p>
+                <Image src="/photo4.jpg" width={700} height={700} />
               </div>
             </div>
             <div className="box photo3">
               <div className="content_box">
                 <h2>photo 3</h2>
-                <p>lorem50</p>
+                <Image src="/photo3.webp" width={700} height={700} />
               </div>
             </div>
           </div>
