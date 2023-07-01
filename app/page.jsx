aed04f23ff26056e781bd6549fa50be5bff1ba44
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-07-01 23:44:56 +0100

    various bg for each section

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,8 @@ import Prices from '@components/Prices'
 export default function Home() {
   return (
     <div className=" h-screen overflow-y-scroll snap-y snap-mandatory ">
-      <main className="flex_center h-screen bg-lime-100 snap-start">
-        <div className="flex flex-col justify-between items-center h-50  bg-slate-300 w-full sm:w-4/5 sm:rounded-xl p-8 ">
+      <main className="flex_center h-screen bg-purple-100 snap-start flex-col bg_barber">
+        <div className="flex flex-col justify-between items-center h-50 opacity-90  bg-slate-300 w-full sm:w-4/5 sm:rounded-xl p-8 ">
           <h2 className="animate__animated animate__bounce">
             Great friendly family barber shop
           </h2>
@@ -14,11 +14,14 @@ export default function Home() {
           <h5>No appointment needed, just pop in</h5>
         </div>
       </main>
-      <section className="flex_center h-screen bg-slate-100 w-full border snap-start ">
+      <section className="flex_center h-screen bg-slate-100 w-full border snap-start bg_clock">
         <OpeningHours />
       </section>
-      <section className="flex_center h-screen bg-lime-100 w-full snap-start ">
+      <section className="flex_center h-screen bg-lime-100 w-full snap-start bg_savings">
         <Prices />
+      </section>
+      <section className="flex_center h-screen bg-blue-100 w-full snap-start ">
+        location
       </section>
     </div>
   )
